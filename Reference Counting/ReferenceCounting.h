eb169479d1490a7d85d825d36606e4f71f185609
double Z <39005525+doubleZ0108@users.noreply.github.com>
2019-10-21 18:25:43 +0800

    ⚗️Reference Counting myString v2.0

--- Reference Counting/ReferenceCounting.h
+++ Reference Counting/ReferenceCounting.h
@@ -16,76 +16,76 @@ using namespace std;
 class myString
 {
 public:
-    friend ostream& operator<<(ostream &os, const myString& str);
-    
-private:
-    struct StringValue{
-        int refCount;
-        char* data;
-        bool shareable;
-        StringValue(const char* initValue);
-        ~StringValue();
-    };
-    StringValue *value = NULL;
-    
-public:
-    myString(){}
-    myString(const char* cstr);
-    
-    myString(const myString& rhs);
-    ~myString();
-    myString& operator=(const myString& rhs);
+	friend ostream& operator<<(ostream& os, const myString& str);
 
-    char* get_c_str() const {return value->data;}
+private:
+	struct StringValue {
+		int refCount;
+		char* data;
+		bool shareable;
+		StringValue(const char* initValue);
+		~StringValue();
+	};
+	StringValue* value = NULL;
+
+public:
+	myString() {}
+	myString(const char* cstr);
+
+	myString(const myString& rhs);
+	~myString();
+	myString& operator=(const myString& rhs);
+
+	char* get_c_str() const { return value->data; }
 };
 
-ostream& operator<<(ostream &os, const myString& str)
+ostream& operator<<(ostream& os, const myString& str)
 {
-    os << str.value->data;
-    return os;
+	os << str.value->data;
+	return os;
 }
 
 myString::StringValue::StringValue(const char* initValue)
-:refCount(1), shareable(true)
+	:refCount(1), shareable(true)
 {
-    data = new char[strlen(initValue) + 1];
-    strcpy(data, initValue);
+	data = new char[strlen(initValue) + 1];
+	strcpy(data, initValue);
 }
 
 myString::StringValue::~StringValue()
 {
-    delete[] data;      //判断计数器为0的语句在外层做了
+	delete[] data;      //判断计数器为0的语句在外层做了
 }
 
 myString::myString(const char* cstr)
-:value(new StringValue(cstr)){}     //唤起StringValue的构造函数
+	:value(new StringValue(cstr)) {}     //唤起StringValue的构造函数
 
 myString::myString(const myString& rhs)
-:value(rhs.value)
+	: value(rhs.value)
 {
-    ++value->refCount;
+	++value->refCount;		//因为这是构造函数,只可能在刚刚创建一个新的变量时才可能被调用,所以不用做相等检测和释放原来的value
 }
 
 myString::~myString()
 {
-    if(--value->refCount == 0){
-        delete value;       //唤起StringValue的析构函数
-    }
+	if (--value->refCount == 0) {
+		delete value;       //唤起StringValue的析构函数
+	}
 }
 
 myString& myString::operator=(const myString& rhs)
 {
-    if(value == rhs.value){
-        return *this;
-    }
-    
-    if(value && --value->refCount == 0){
-        delete value;
-    }
-    
-    value = rhs.value;
-    ++value->refCount;
-    return *this;
+	if (value == rhs.value) {
+		return *this;
+	}
+
+	if (value && --value->refCount == 0) {
+		delete value;
+	}
+
+	value = rhs.value;		//二者的类型都是指针
+	++value->refCount;		//递增this->value的refCount, rhs.value的refCount也同步变化
+	return *this;
 }
 
 #endif /* ReferenceCounting_h */
